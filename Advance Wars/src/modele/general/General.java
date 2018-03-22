d4e0d8c7a34be4a6c7a82818961b68951d04aefb
MadCindy <cindypereira93@gmail.com>
2018-03-22 09:42:33 +0100

    Changement du gain à la mort d'une unité.

--- Advance Wars/src/modele/general/General.java
+++ Advance Wars/src/modele/general/General.java
@@ -9,7 +9,7 @@ import src.modele.interfaces.deplacement.Deplacement;
 public abstract class General extends AbstractUnite {
 
   public General (String n, int pM, Combat c, Deplacement d, int dis, int vis, int ess, TypeUnite t, Joueur j, int x, int y, int i) {
-    super(n, pM, c, d, dis, 3, vis, ess, 0, t, j, x, y, i);
+    super(n, pM, c, d, dis, 3, vis, ess, 30000, t, j, x, y, i);
   }
 
   abstract void bonusPersonel();
--- Advance Wars/src/vue/Jeu.java
+++ Advance Wars/src/vue/Jeu.java
@@ -45,10 +45,7 @@ public class Jeu {
     if (cible.getPV() <= 0) {
       cible.getJoueur().remove(cible);
       map.rmvUnite(cible);
-        if (cible instanceof General)
-          unite.getJoueur().setArgent(10000);
-        else
-          unite.getJoueur().setArgent(1000);
+        unite.getJoueur().setArgent(unite.getGainMort());
     }
   }
 
